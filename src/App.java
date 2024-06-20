b9bebcaed2a3e6ff16e5989e6db81b67b9cf4180
Giovanni Cappelletti <tech@methodwolf.com>
2024-06-20 10:41:57 +0200

    init

---
+++ src/App.java
@@ -0,0 +1,36 @@
+public class App {
+
+    /**
+     * TODO:
+     * 
+     * 1. Creare una classe `Veicolo` con i seguenti attributi:
+     * - `marca` (String)
+     * - `modello` (String)
+     * 
+     * 2. Creare una classe `Auto` che estende `Veicolo`
+     * e aggiungere i seguenti attributi:
+     * - `cilindrata` (int)
+     * - `potenza` (int)
+     * 
+     * 3. Creare una classe `Camion` che estende `Auto`
+     * e aggiungere i seguenti attributi:
+     * - `carico` (int)
+     * - `rimorchio` (boolean)
+     * 
+     * 4. Creare una classe `Moto` che estende `Veicolo`
+     * e aggiungere i seguenti attributi:
+     * - `bauletto` (boolean)
+     * - `casco` (boolean)
+     * 
+     * 5. Definire un toString sensato per ogni classe
+     * 
+     * 6. Creare delle istanze di `Veicolo`, `Auto`,
+     * `Camion` e `Moto` e testare il corretto funzionamento
+     * di ogni classe
+     * 
+     */
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
